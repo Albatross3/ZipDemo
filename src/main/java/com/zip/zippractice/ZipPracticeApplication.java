16256cba4dc48d704ff7b632a22893905b5f73e4
DongJunLee <superstring77@gmail.com>
2022-06-22 21:08:34 +0900

    first commit

---
+++ src/main/java/com/zip/zippractice/ZipPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.zip.zippractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zip/zippractice/ZipPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zip.zippractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
