552b37274332c75528a76fdeb3837a09a31261cb
DongJunLee <superstring77@gmail.com>
2022-06-26 23:57:38 +0900

    6/26 등록/수정/조회 API 만들기

---
+++ src/main/java/com/zip/zippractice/web/PostsApiController.java
@@ -0,0 +1,33 @@
+package com.zip.zippractice.web;
+
+import com.zip.zippractice.service.posts.PostsService;
+import com.zip.zippractice.web.dto.PostsResponseDto;
+import com.zip.zippractice.web.dto.PostsSaveRequestDto;
+import com.zip.zippractice.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    // 등록
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto){
+        return postsService.save(requestDto);
+    }
+
+    // 수정
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id,requestDto);
+    }
+
+    // 조회
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
+    }
+}
