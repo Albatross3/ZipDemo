8e188e8928f427370ff8d43ab27ceed384ad1b52
DongJunLee <superstring77@gmail.com>
2022-07-07 22:03:20 +0900

    7/7 구글 로그인 기능 완료

--- src/main/java/com/zip/zippractice/web/IndexController.java
+++ src/main/java/com/zip/zippractice/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.zip.zippractice.web;
 
+import com.zip.zippractice.config.auth.LoginUser;
+import com.zip.zippractice.config.auth.dto.SessionUser;
 import com.zip.zippractice.service.posts.PostsService;
 import com.zip.zippractice.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,14 +10,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postsService.findAllDesc());
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
